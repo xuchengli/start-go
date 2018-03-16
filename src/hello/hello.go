4fddabe65a628e66764d8f7b0e6bf08fb243ce7c
lixucheng <lixucheng@aliyun.com>
2018-03-16 14:54:14 +0800

    revert gitignore

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("hello, world")
 	fmt.Println("My favorite number is: ", rand.Intn(10))
+	fmt.Println(math.Pi)
 }
