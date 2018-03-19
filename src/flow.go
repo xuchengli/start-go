f7a5e9600b5b7409cd326a756daaf7397738387c
lixucheng <lixucheng@aliyun.com>
2018-03-19 14:25:57 +0800

    complete flow control

--- src/flow.go
+++ src/flow.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 func sqrt(x float64) string {
@@ -35,4 +37,31 @@ func main() {
 	fmt.Println(sqrt(2), sqrt(-4))
 
 	fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+
+	fmt.Print("Go runs on: ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS x.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s.", os)
+	}
+
+	t := time.Now()
+	fmt.Println(t)
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning.")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	fmt.Println("Counting>>>")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("Done.")
 }
